e06b5b9203e1ff0e22e0f9ac381331303ef56579
rikita <rikitamaharjan@gmail.com>
2021-07-01 23:26:08 +0545

    User

---
+++ app/src/main/java/com/example/ktmeats/ViewHolder/MenuViewHolder.java
@@ -0,0 +1,54 @@
+package com.example.ktmeats.ViewHolder;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.ktmeats.FoodList;
+import com.example.ktmeats.Interface.ItemClickListener;
+import com.example.ktmeats.Model.Category;
+import com.example.ktmeats.R;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+
+public class MenuViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public TextView txtMenuName;
+    public ImageView imageView;
+
+    public TextView item;
+    FirebaseRecyclerAdapter<Category,MenuViewHolder> adapter;
+
+
+    private ItemClickListener itemClickListener;
+
+
+    public MenuViewHolder(View itemView) {
+        super(itemView);
+
+        item = (TextView) itemView.findViewById(R.id.menu_item);
+        txtMenuName = (TextView) itemView.findViewById(R.id.menu_name);
+        imageView = (ImageView) itemView.findViewById(R.id.menu_image);
+
+    }
+
+    public void setItemClickListener(ItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+//        Intent intent = new Intent(v.getContext(), FoodList.class);
+//        intent.putExtra("CategoryId", item.getText().toString());
+//        v.getContext().startActivity(intent);
+
+        itemClickListener.onClick(v,getAdapterPosition(),false);
+
+    }
+
+}
